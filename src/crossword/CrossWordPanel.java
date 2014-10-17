f6ad07da8f4c744e207b24aef52dc54cd2b49770
unknown <reiliuyan@gmail.com>
2014-10-17 15:14:45 -0400

    Panel constructor

--- src/crossword/CrossWordPanel.java
+++ src/crossword/CrossWordPanel.java
@@ -6,9 +6,29 @@
 package crossword;
 
 /**
- *
+ * This class represents a n*n cross word panel to accommodate words.
+ * 
  * @author Yan Liu
+ * @version 1.0
+ * 10/17/2014
  */
 public class CrossWordPanel {
+    private char[][] panel;
+    
+    /**
+     * Constructor with no argument
+     */
+    public CrossWordPanel(){
+    }
+    
+    /**
+     * Constructor initializes the panel with required size.
+     * 
+     * @param n the number of grids in a row / column. 
+     */
+    public CrossWordPanel(int n){
+        panel = new char[n][n];
+    }
+    
     
 }
