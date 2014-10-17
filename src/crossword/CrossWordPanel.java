8c6ef1de1fbbf5a3bf24454601c00a0f0f7f739f
unknown <reiliuyan@gmail.com>
2014-10-17 15:31:03 -0400

    Show panel

--- src/crossword/CrossWordPanel.java
+++ src/crossword/CrossWordPanel.java
@@ -24,11 +24,57 @@ public class CrossWordPanel {
     /**
      * Constructor initializes the panel with required size.
      * 
-     * @param n the number of grids in a row / column. 
+     * @param n the number of grids in a row / column
      */
     public CrossWordPanel(int n){
         panel = new char[n][n];
     }
     
+    /**
+     * Set the char of grid by row and column number.
+     * @param row row number
+     * @param column column number
+     * @param c the char to be set
+     */
+    public void setGrid(int row, int column, char c){
+        panel[row][column] = c;
+    }
     
+    /**
+     * Prints out the whole panel. 
+     */
+    public void showPanel(){
+        printLine(panel.length);
+        for (int i=0; i<panel.length;i++){
+            System.out.print("|");
+            for (int j=0; j<panel.length;j++){
+                System.out.print(" "+panel[i][j]+" ");
+                System.out.print("|");
+            }
+            System.out.print("\n");
+            printLine(panel.length);
+        }
+    }
+    
+    /**
+     * Prints a line.
+     * @param n the number of grids in a row / column
+     */
+    private void printLine(int n){
+        for (int i=0; i<n; i++){
+            System.out.print("----");
+        }
+        System.out.print("\n");
+    }
+    
+    public static void main(String[] args){
+        CrossWordPanel cwp = new CrossWordPanel(5);
+        for (int i=0; i<5;i++){
+            for (int j=0; j<5;j++){
+                cwp.setGrid(i, j, 'a');
+            }
+        }
+        cwp.showPanel();
+        
+    }
 }
